e9cd6176467863693a348ce8b1b1b42b2d2626e1
felipe <felipe@sti.ufpb.br>
2026-04-27 17:15:47 -0300

    log adicionado ao projeto

--- src/main/java/br/com/erudio/services/PersonServices.java
+++ src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,19 +1,16 @@
 package br.com.erudio.services;
-
 import br.com.erudio.exception.ResourceNotFoundException;
 import br.com.erudio.model.Person;
 import br.com.erudio.repository.PersonRepository;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.logging.Logger;
-
 @Service
 public class PersonServices {
 
-    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
 	@Autowired
 	PersonRepository repository;
